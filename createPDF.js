6b73e11ab452e6d0ffcb8714441a7bc9afe550c5
Stiven <strujillodev@gmail.com>
2023-05-03 15:15:27 -0500

    change columns

--- createPDF.js
+++ createPDF.js
@@ -18,13 +18,13 @@ async function gerenratePDF() {
   // const element = document.body // Delete this
   const opt = {
     margin: 0,
-    filename: "cv.pdf",
+    filename: "cv-Stiven.pdf",
     image: { type: "jpeg", quality: 1 },
     html2canvas: { scale: 5 }, // entre mas alto mas definicion
     enableLinks: true,
     jsPDF: {
       unit: "mm", // or px or cm
-      format: [210, 340], // or letter or a4 or legal or ledger, [210, 330] mm
+      format: [210, 360], // or letter or a4 or legal or ledger, [210, 330] mm
       orientation: "portrait",
       precision: "16", // or 12
     },
