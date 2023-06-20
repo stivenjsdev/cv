bc2b8d2ab978b6ad8c6b03d1cab4a22d0031b543
Stiven <strujillodev@gmail.com>
2023-06-20 10:36:21 -0500

    feat: fix function name, and change size

--- createPDF.js
+++ createPDF.js
@@ -1,4 +1,4 @@
-async function gerenratePDF() {
+async function generatePDF() {
   console.log("creating pdf...");
 
   /**
@@ -18,13 +18,13 @@ async function gerenratePDF() {
   // const element = document.body // Delete this
   const opt = {
     margin: 0,
-    filename: "cv-Stiven.pdf",
+    filename: "stivenCv.pdf",
     image: { type: "jpeg", quality: 1 },
     html2canvas: { scale: 5 }, // entre mas alto mas definicion
     enableLinks: true,
     jsPDF: {
       unit: "mm", // or px or cm
-      format: [210, 360], // or letter or a4 or legal or ledger, [210, 330] mm
+      format: [210, 550], // or letter or a4 or legal or ledger, [210, 330] mm
       orientation: "portrait",
       precision: "16", // or 12
     },
@@ -42,4 +42,4 @@ async function gerenratePDF() {
 }
 
 const button = document.getElementById("export-button");
-button.addEventListener("click", gerenratePDF);
+button.addEventListener("click", generatePDF);
